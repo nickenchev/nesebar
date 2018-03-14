50ba030ba1907ce184f1adb45b3839dd1242c5f5
Nick Enchev <nikolaenchev@gmail.com>
2018-03-14 01:19:46 -0400

    Loading mapper # and flags

--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,21 @@
 #include <iomanip>
 #include <fstream>
 #include <string>
+#include <bitset>
 #include "cpu.h"
 
 using byte = uint8_t;
 
+static const int prgRomPageSize = 16384;
+static const int chrRomPageSize = 8192;
+static const int flag6FourScreenMode = 3;
+static const int flag6Trainer = 2;
+static const int flag6SRAMBattery = 1;
+static const int flag6Mirroring = 0;
+
 struct Header
 {
-	char code[3];
+	char code[4];
 	byte prgRomSize;
 	byte chrRomSize;
 	byte flag6;
@@ -17,6 +25,15 @@ struct Header
 	byte flag9;
 	byte flag10;
 	byte zeroFilled[5];
+
+	Header()
+	{
+		for (int i = 0; i < 4; ++i) code[i] = 0;
+		prgRomSize = 0;
+		chrRomSize = 0;
+		flag6 = flag7 = flag9 = flag10 = 0;
+		for (int i = 0; i < 5; ++i) zeroFilled[i] = 0;
+	}
 };
 
 int main(int argc, const char *argv[])
@@ -31,6 +48,24 @@ int main(int argc, const char *argv[])
 			std::cout << "Running: " << path << std::endl;
 			Header header;
 			romFile.read((char *)&header, sizeof(Header));
+
+			std::bitset<8> flag6(header.flag6);
+			std::bitset<8> flag7(header.flag7);
+
+			std::bitset<8> mapperBits(0);
+			for (int i = 4; i < 8; ++i)
+			{
+				if (flag6.test(i))
+				{
+					mapperBits.set(i);
+				}
+				if (flag7.test(i))
+				{
+					mapperBits.set(i + 4);
+				}
+			}
+			byte mapper = static_cast<byte>(mapperBits.to_ulong());
+			bool hasTrainer = flag6.test(flag6Trainer);
 
 			while (!romFile.eof())
 			{
